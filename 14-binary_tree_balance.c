7f5a879c314e68b28031c49885e1746632c9d5aa
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2021-07-21 12:09:04 -0500

    [balance] Fixed!

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,23 @@
 #include "binary_trees.h"
 
+/***/
+int binary_tree_balance_recursive(const binary_tree_t *tree)
+{
+	int heightr = 0;
+	int heightl = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
+	if (tree->left)
+		heightl = 1 + binary_tree_balance_recursive(tree->left);
+	if (tree->right)
+		heightr = 1 + binary_tree_balance_recursive(tree->right);
+
+	return (heightl > heightr ? heightl : heightr);
+}
+
 /**
  *binary_tree_balance - calculate the balance of the tree
  *@tree: Node
@@ -9,10 +27,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->parent)
-		return (binary_tree_balance(tree->left) -
-			binary_tree_balance(tree->right));
-
-	return (1 + binary_tree_balance(tree->left) -
-		binary_tree_balance(tree->right));
+	return (binary_tree_balance_recursive(tree->left) -
+		binary_tree_balance_recursive(tree->right));
 }
